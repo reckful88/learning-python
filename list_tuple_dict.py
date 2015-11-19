33cf4352c16dc118e31435c3411cf519f9d6be2f
lifangcheng <luosuodui@163.com>
2015-11-19 14:29:27 +0800

    list tuple dict string 相互转换

--- list_tuple_dict.py
+++ list_tuple_dict.py
@@ -1,6 +1,24 @@
 # -*- coding: utf-8 -*-
 
 '''
-list tuple 转为dict类型
+list tuple 转为dict类型 互相转换
 '''
+mytuple = (1,2,3)
+print list(mytuple)     #Tuple to list
+
+mylist = [1,2,3]
+print tuple(mylist)     #List to tuple
+
+
+mylist2 = [('blue',5),('red',3),('yellow',7)]
+print dict(mylist2)     #List to dict
+
+
+mystring = 'hello'
+print list(mystring)    #String to list
+
+
+mylist3 = ['w','or','ld']
+print ''.join(mylist3)  #List to string
+
 
