1694f2aeb38de4c1b816be83384150ad64f85469
lifangcheng <luosuodui@163.com>
2015-11-22 14:23:50 +0800

    计算任意一个数的 任意次方

---
+++ lianxi_def_ncf.py
@@ -0,0 +1,25 @@
+#-*- coding: utf-8 -*-
+
+'''
+计算任意一个数的n次方
+2，3  就是2的3次方
+'''
+
+def power(x,y):
+    return x ** y
+
+print power (2,3)
+
+
+'''
+方法二：
+def power(x,y):
+    s = 1
+    while n > 0:
+        n = n -1
+        s = s * x
+    return s
+'''
+
+
+
