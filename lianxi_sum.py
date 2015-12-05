e331286aee95a16df40993aa1cdc987c9b1e3098
lifangcheng <luosuodui@163.com>
2015-12-05 15:40:19 +0800

    循环和递归 两种方法 求和

--- lianxi_sum.py
+++ lianxi_sum.py
@@ -5,3 +5,16 @@ print sum
 
 
 ########1+2+3+.....+10################
+
+'''
+用递归的方法
+
+def sums(n):
+    if n == 0 or n == 1:
+        return 1
+    else:
+        return n + sums(n-1)
+
+print sums(3)
+
+'''
