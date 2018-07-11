0e0de57267d27a82279f72539d1a07d6a16a3716
lifangcheng <lifangcheng-rj@ofo.com>
2018-07-11 16:21:39 +0800

    alex

---
+++ devspace/day1/raw_Input.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# Author: lifangcheng
+
+username = input("YourName:")
+password = input("YourPasswd:")
+
+print(username, password)
---
+++ devspace/day1/var_unicode.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# Author: lifangcheng
+
+'''
+变量和字符编码
+'''
+
+name  = "lfc"
+
+name2 = name
+
+print("My name is", name, name2)
+
+name =  "new-lfc"
+
+print('My name is', name, name2)
+
+# 常量的变量名要大写
+PIE = 3.1415926
+
+print(PIE)
+
+
+# 打印多行
+msg = '''
+aaaa = 123
+xxxx
+ccc = 'sadadada'
+'''
+
+print(msg)
