3432cfcd18d39ce61eb62fe344b2937ebfb1bf26
lifangcheng <lifangcheng-rj@ofo.com>
2018-07-11 18:38:57 +0800

    三种格式化输出的方式 input

--- devspace/day1/Input.py
+++ devspace/day1/Input.py
@@ -8,7 +8,7 @@ password = input("YourPasswd:")
 print(username, password)
 
 
-# 格式化输出
+# 格式化输出，三种不同的方式
 
 name = input("YourName:")
 age = int(input("YourAge:"))
@@ -23,3 +23,23 @@ Job: %s
 ''' % (name, name, age, job)
 
 print(info)
+
+info2 = '''
+-------- info of {_name} -------
+Name: {_name}
+Age: {_age}
+Job: {_job}
+''' .format(_name = name,
+            _age = age,
+            _job = job)
+
+print(info2)
+
+info3 = '''
+-------- info of {0} -------
+Name: {0}
+Age: {1}
+Job: {2}
+''' .format(name, name, age, job)
+
+print(info3)
