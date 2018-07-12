09a700ff26edbf37bbeb83e14dd4a6a9f8d53171
lifangcheng <lifangcheng-rj@ofo.com>
2018-07-12 17:26:39 +0800

    while 循环优化

--- devspace/day1/guess_game.py
+++ devspace/day1/guess_game.py
@@ -3,6 +3,9 @@
 # Author: lifangcheng
 
 
+'''
+# 版本1
+
 age_of_lfc = 26
 
 count = 0
@@ -18,3 +21,22 @@ while True:
     else:
         print("too old")
     count = count +1
+'''
+
+# 版本2
+
+age_of_lfc = 26
+
+count = 0
+while count < 3:
+    guess_age = int(input("guess a age: "))
+    if guess_age == age_of_lfc:
+        print("You are Right.")
+        break
+    elif guess_age > age_of_lfc:
+        print("Too Old..")
+    else:
+        print("Too Young..")
+    count = count +1
+else:
+    print("You geuss too many...")
