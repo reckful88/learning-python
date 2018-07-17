b185bbd97279c0942a752f32103eb860c95aa0ff
lifangcheng <lifangcheng-rj@ofo.com>
2018-07-17 16:39:25 +0800

    作业1：登陆接口

--- devspace/day1/homework1.py
+++ devspace/day1/homework1.py
@@ -10,28 +10,42 @@
 * 输错三次后锁定
 '''
 
-_user = 'lfc'
-_passwd = '123456'
 
-with open('/Users/lifangcheng/ENV3.6/learning-python/devspace/day1/blacklist', 'r') as f:
-    blacklist = f.read()
+with open('/Users/lifangcheng/ENV3.6/learning-python/devspace/day1/userpasswd', 'r') as f1:
+    userpasswd = f1.readlines()
 
-count = 0
-while count < 3:
+with open('/Users/lifangcheng/ENV3.6/learning-python/devspace/day1/blacklist', 'r') as f2:
+    blacklist = f2.readlines()
+
+for x in range(3):
     username = input('请输入用户名：')
     passwd = input('请输入密码：')
 
-    if username in blacklist:
-        print("您的账号已被锁定，请联系管理员解锁.")
+    flag1 = False
+    for j in blacklist:
+        j = j.strip()
+        if username == j:
+            print("您的账号已被锁定，请联系管理员解锁.")
+            flag1 = True
+            break
+    if flag1 == True:
         break
 
-    if username == _user and passwd == _passwd:
-        print('登录成功，欢迎~')
+    flag = False
+    for i in userpasswd:
+        i = i.strip()
+        a, b = i.split(":")
+        if a == username and b == passwd:
+            print("登陆成功，欢迎 ~")
+            flag = True
+            break
+    if flag == True:
         break
+
     else:
         print('登录失败，请检查用户名或密码是否正确！')
-        count = count +1
+
 else:
     print('尝试错误次数过多，账号已被锁定！')
-    with open('/Users/lifangcheng/ENV3.6/learning-python/devspace/day1/blacklist', 'a') as f:
-        f.write(username + '\n')
+    with open('/Users/lifangcheng/ENV3.6/learning-python/devspace/day1/blacklist', 'a') as f2:
+        f2.write(username + '\n')
---
+++ devspace/day1/homework2.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# Author: lifangcheng
+
+'''
+多级菜单
+
+* 三级菜单
+* 可依次选择进入各子菜单
+* 所需新知识点：列表、字典
+'''
+
