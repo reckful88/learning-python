6e71b48dc8233341e2d8de810a755828f3350a9b
lifangcheng <lifangcheng-rj@ofo.com>
2018-08-02 15:32:27 +0800

    购物车优化, 未完成, 暂存

--- devspace/day2/homework_shopping_cart2.py
+++ devspace/day2/homework_shopping_cart2.py
@@ -35,18 +35,18 @@ salary = input('请输入您的工资: ')
 
 shopping_list = []
 
-if salary.isdigit():       # 判断是否为整数
+if salary.isdigit():  # 判断是否为整数
     salary = int(salary)
     while True:
-        for index, item in enumerate(product_list):     #  通过列表的角标作为商品编号
-            #print(product_list.index(item), item)
+        for index, item in enumerate(product_list):  # 通过列表的角标作为商品编号
+            # print(product_list.index(item), item)
             print(index, item)
         user_choice = input('根据商品编号选择您要购买的商品, 按 q 退出>>>: ')
-        if user_choice.isdigit():   # 判断输入的是否是数字
+        if user_choice.isdigit():  # 判断输入的是否是数字
             user_choice = int(user_choice)
-            if user_choice < len(product_list) and user_choice >= 0:    # 输入的商品编号不能超过范围
-                p_item = product_list[user_choice]      # 通过角标取到商品
-                if p_item[1] <= salary:     # 判断商品价格小于用户工资
+            if user_choice < len(product_list) and user_choice >= 0:  # 输入的商品编号不能超过范围
+                p_item = product_list[user_choice]  # 通过角标取到商品
+                if p_item[1] <= salary:  # 判断商品价格小于用户工资
                     shopping_list.append(p_item)
                     salary = salary - p_item[1]
                     print('商品: %s 已经放入了您的购物车中, 您的当前工资余额为: \033[32;1m%s\033[0m' % (p_item, salary))
