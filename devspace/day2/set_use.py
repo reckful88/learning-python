2fe7073efb5331869e7888c9fb8ddf0e67f003fc
lifangcheng <lifangcheng-rj@ofo.com>
2018-08-10 15:25:23 +0800

    集合的使用

--- devspace/day2/set_use.py
+++ devspace/day2/set_use.py
@@ -16,23 +16,37 @@ print(name)
 set_name = set(name)
 print(set_name, type(set_name))
 
-
 # 取交集(合并起来找到重复的)
 
-list1 = [1, 3, 2, 6, 7, 3, 4 ]
+list1 = [1, 3, 2, 6, 7, 3, 4]
 list2 = [9, 4, 5, 7, 3, 6, 1]
 print(list1, list2)
 print(set(list1).intersection(set(list2)))
 
-
 # 并集(合并起来去掉重复的)
 
-list1 = [1, 3, 2, 6, 7, 3, 4 ]
+list1 = [1, 3, 2, 6, 7, 3, 4]
 list2 = [9, 4, 5, 7, 3, 6, 1]
 print(set(list1).union(set(list2)))
 
 # 差集(我有你没有,或者你有我没有的元素,找出不同)
 
-list1 = [1, 3, 2, 6, 7, 3, 4 ]
+list1 = [1, 3, 2, 6, 7, 3, 4]
 list2 = [9, 4, 5, 7, 3, 6, 1]
 print(set(list1).difference(set(list2)))
+print(set(list2).difference(set(list1)))
+
+# 子集(是否包含一个集合)
+
+list1 = [1, 3, 2, 6, 7, 3, 4]
+list2 = [9, 4, 5, 7, 3, 6, 1]
+print(set(list1).issubset(set(list2)))  # 判断 list2 是不是 list1 的子集
+
+# 父集
+
+list1 = [1, 3, 2, 6, 7, 3, 4]
+list2 = [9, 4, 5, 7, 3, 6, 1]
+print(set(list1).issuperset(set(list2)))  # 判断 list2 是不是 list1 的父集
+
+# 对称差集
+print(set(lit1).symmetric_difference(set(list2)))  # 取出这两个集合内都互相没有的元素
